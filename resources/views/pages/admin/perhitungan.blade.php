55726a8a4a78f3e1d3f77b12169ce697ef3fa407
mi-codex <irvanboy29@gmail.com>
2021-12-20 11:34:33 +0700

    merubah total tampilan dan mengambil data dari mysql

--- resources/views/pages/admin/perhitungan.blade.php
+++ resources/views/pages/admin/perhitungan.blade.php
@@ -3,92 +3,125 @@
 @section('judul', 'Perhitungan')
 
 @section('css')
-    <link rel="stylesheet" href="//cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="//cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
 @endsection
 
 @section('konten')
 
 
-    <!-- Header -->
-    <div class="header bg-primary pb-6">
-        <div class="container-fluid">
-            <div class="header-body">
-                <div class="row align-items-center py-4">
-                    <div class="col">
-                        <h1 class=" text-white">Perhitungan</h1>
-                    </div>
+<!-- Header -->
+<div class="header bg-primary pb-6">
+    <div class="container-fluid">
+        <div class="header-body">
+            <div class="row align-items-center py-4">
+                <div class="col">
+                    <h1 class=" text-white">Perhitungan</h1>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
-    <!-- Content -->
-    <div class="container-fluid mt--6">
-        <div class="row justify-content-center">
-            <div class="col">
-                <div class="card">
-                    <div class="card-header bg-transparent">
-                        <h3 class="mb-0">Data Mahasiswa</h3>
-                    </div>
-                    <div class="card-body">
+<!-- Content -->
+<div class="container-fluid mt--6">
+    <div class="row justify-content-center">
+        <div class="col">
+            <div class="card">
+                <div class="card-header bg-transparent">
+                    <h3 class="mb-0">Data Mahasiswa</h3>
+                </div>
+                <div class="card-body">
 
-                        <!-- Konten  -->
-                        <div class="row mb-3">
-                            <div class="col">
-                                <!-- Data Set -->
-                                <h3 class="text-success">Data Set</h3> <br>
-                                <button type="button" class="btn btn-success" data-toggle="modal"
-                                    data-target="#exampleModal">
-                                    Import Data
-                                </button>
+                    <!-- Konten  -->
+                    <div class="row mb-3">
+                        <div class="col">
+                            <!-- Data Set -->
+                            <h3 class="text-success">Data Set</h3> <br>
+                            <button type="button" class="btn btn-success" data-toggle="modal"
+                                data-target="#exampleModal">
+                                Import Data
+                            </button>
 
-                                <!-- Modal -->
-                                <div class="modal fade" id="exampleModal" tabindex="-1"
-                                    aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="exampleModalLabel">Import Data</h5>
-                                                <button type="button" class="close" data-dismiss="modal"
-                                                    aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <form>
-                                                    <div class="form-group">
-                                                        <label for="exampleFormControlFile1">Masukkan File Excel</label>
-                                                        <input type="file" class="form-control-file"
-                                                            id="exampleFormControlFile1" name="fileexcel">
-                                                    </div>
-                                                </form>
-                                                <a href="/downloadfile" class="text-sm text-success"> >>Download Contoh
-                                                    Format
-                                                    Excel</a>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary"
-                                                    data-dismiss="modal">Close</button>
-                                                <button type="button" class="btn btn-success">Kirim Data</button>
-                                            </div>
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                                aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Import Data</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form>
+                                                <div class="form-group">
+                                                    <label for="exampleFormControlFile1">Masukkan File Excel</label>
+                                                    <input type="file" class="form-control-file"
+                                                        id="exampleFormControlFile1" name="fileexcel">
+                                                </div>
+                                            </form>
+                                            <a href="/downloadfile" class="text-sm text-success"> >>Download Contoh
+                                                Format
+                                                Excel</a>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary"
+                                                data-dismiss="modal">Close</button>
+                                            <button type="button" class="btn btn-success">Kirim Data</button>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col">
+                    </div>
 
-                            </div>
+                    <div class="row">
+                        <div class="col">
+                            <table class="table table-responsive">
+                                <thead class="thead-light">
+                                    <tr>
+                                        <th>NO.</th>
+                                        <th>NAMA MAHASISWA</th>
+                                        <th>PENANGGUNG</th>
+                                        <th>PENGHASILAN</th>
+                                        <th>KEPEMILIKAN RUMAH</th>
+                                        <th>JENIS KELAMIN</th>
+                                        <th>PEKERJAAN</th>
+                                        <th>JUMLAH TANGGUNGAN</th>
+                                        <th>USIA</th>
+                                        <th>NILAI UN</th>
+                                        <th >STATUS BEASISWA</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($items as $item)
+                                    <tr>
+                                        <td>{{ $item->id }}</td>
+                                        <td>{{ $item->nama_mhs }}</td>
+                                        <td>{{ $item->penanggung }}</td>
+                                        <td>{{ $item->penghasilan }}</td>
+                                        <td>{{ $item->rumah }}</td>
+                                        <td>{{ $item->jenis_kelamin }}</td>
+                                        <td>{{ $item->pekerjaan }}</td>
+                                        <td>{{ $item->jumlah_tanggungan }}</td>
+                                        <td>{{ $item->usia }}</td>
+                                        <td>{{ $item->nilai_un }}</td>
+                                        <td class="text-success font-weight-bold">{{ $item->status_beasiswa }}</td>
+                                    </tr>
+                                    @endforeach
+
+                                </tbody>
+                            </table>
                         </div>
                     </div>
-                    <!-- End Of card-body -->
                 </div>
             </div>
+            <!-- End Of card-body -->
         </div>
     </div>
-    <!-- End of Content -->
+</div>
+</div>
+<!-- End of Content -->
 
 @endsection
-
-
