2755694f1e9df5040138cfef86253294c75eb99a
mi-codex <irvanboy29@gmail.com>
2021-12-22 13:51:46 +0700

    merubah tampilan panduan

--- resources/views/pages/v_panduan.blade.php
+++ resources/views/pages/v_panduan.blade.php
@@ -47,6 +47,7 @@
                                 <li>Klik Menu "Perhitungan"</li>
                                 <li>Import Datasets yang diberikan</li>
                                 <li>Export Datasets bila diperlukan</li>
+                                <li>Contoh Format Datasets Excel terdapat pada Folder Aplikasi Laravel di Folder Public dengan Nama file datasets.xlsx</li>
                                 <li>Setelah Mengimport data, Maka dataset yang anda import akan muncul dibawah kedua tombol tersebut</li>
                                 <li>Pastikan Sebelum Melakukan Perhitungan Cek Data Masuk Terlebih dahulu</li>
                             </ol>
@@ -64,6 +65,7 @@
                             <p class="text-danger font-weight-bold">PERHATIAN: Isi data anda secara jujur! dan bila data
                                 yang diberikan tidak sesuai maka akan ada sanksi yang anda terima. Aplikasi ini tidak hanya
                                 ditentukan oleh nilai UN Anda.</p>
+                            <p class="text-yellow font-weight-bold">Contact Admin : Admin@gmail.com</p>
                         </div>
                         @endif
 
